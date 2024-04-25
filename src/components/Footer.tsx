3f1414eaf17c33bf24236994d1f96b230a883ddb
Bendako <62848655+Bendako@users.noreply.github.com>
2024-04-25 20:29:07 +0300

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Footer({}: Props) {
+  return (
+    <footer className="footer mt-4 footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+            <p>Copyright © 2024 - All right reserved by BD</p>
+        </aside>
+    </footer>
+  )
+}
